053f67cc9479b928950231c2d7a18752c0152935
Konstantin Raev <kos_walker@mail.ru>
2023-04-10 10:22:37 +0300

    2

--- Java_mvc/sweater_v1.0/src/main/java/org/example/sweater_v1/GreetingController.java
+++ Java_mvc/sweater_v1.0/src/main/java/org/example/sweater_v1/GreetingController.java
@@ -1,14 +1,21 @@
 package org.example.sweater_v1;
 
+import org.example.sweater_v1.domain.Message;
+import org.example.sweater_v1.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+
+    @Autowired
+    private MessageRepo messageRepo;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
@@ -20,7 +27,21 @@ public class GreetingController {
 
     @GetMapping("/")
     public String main(Map<String, Object> model) {
-        model.put("some", "hello, lets Code!");
+       Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
+
         return "main";
     }
 }
