a695e00b77333a85ef228f6d4e530dd0aa17e513
Konstantin Raev <kos_walker@mail.ru>
2023-04-14 11:44:54 +0300

    Less 3 Spring FW in process!

--- Java_mvc/sweater_v1.0/src/main/java/org/example/sweater_v1/config/WebSecurityConfig.java
+++ Java_mvc/sweater_v1.0/src/main/java/org/example/sweater_v1/config/WebSecurityConfig.java
@@ -1,44 +1,43 @@
 package org.example.sweater_v1.config;
 
-import org.springframework.context.annotation.Bean;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+    @Autowired
+    private DataSource dataSource;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/main").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout((logout) -> logout.permitAll());
-
-        return http.build();
+                .authorizeHttpRequests()
+                    .requestMatchers("/", "/registration").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                .and()
+                    .logout()
+                    .permitAll();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .usersByUsernameQuery("select username, password, active from usr where user username=?")
+                .authoritiesByUsernameQuery("select u.username, ur.roles, from usr u inner join user_role ur on u.id=ur.user_id where u.username=?");
     }
 }
--- Java_mvc/sweater_v1.0/src/main/java/org/example/sweater_v1/controller/MainController.java
+++ Java_mvc/sweater_v1.0/src/main/java/org/example/sweater_v1/controller/MainController.java
@@ -1,10 +1,9 @@
-package org.example.sweater_v1;
+package org.example.sweater_v1.controller;
 
 import org.example.sweater_v1.domain.Message;
 import org.example.sweater_v1.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
 
     @Autowired
     private MessageRepo messageRepo;
@@ -59,4 +58,6 @@ public class GreetingController {
 
         return "main";
     }
+
+
 }
